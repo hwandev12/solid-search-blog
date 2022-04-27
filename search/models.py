43535911290e8693fb943c2527481ffb334be918
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-27 21:26:58 +0500

    create workers model

--- search/models.py
+++ search/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Workers(models.Model):
+    class Meta:
+        verbose_name = 'Workers'
+        verbose_name_plural = 'My Workers'
+
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField(default=18)
+    profession = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
