85a8daaa01ed0bc297077b615e5580cb61dcd8eb
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-28 10:44:26 +0500

    create queryset and context data

--- search/views.py
+++ search/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
+from .models import Workers
 
-
-class BaseTemplateView(generic.TemplateView):
+class BaseTemplateView(generic.ListView):
     template_name = 'pages/home.html'
+    queryset = Workers.objects.all()
+    context_object_name = 'workers'
