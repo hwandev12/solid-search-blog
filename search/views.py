dd38a99512a9ab749c905b2d779ce70370e8c974
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-28 11:24:00 +0500

    search functionality better view

--- search/views.py
+++ search/views.py
@@ -1,13 +1,18 @@
-from multiprocessing import context
 from django.shortcuts import render
 from django.views import generic
 from .models import Workers
+from django.db.models import Q
 
 
 def home(request):
+    # In this method we can search anything related to following
+    # We have used Q from imports
+    # we have used something__icontains to search related to something
+    # we have used filter method and request method as following
     if 'q' in request.GET:
         search_key = request.GET['q']
-        workers = Workers.objects.filter(name__icontains=search_key)
+        full_search = Q(Q(name__icontains=search_key) | Q(last_name__icontains=search_key) | Q(profession__icontains=search_key))
+        workers = Workers.objects.filter(full_search)
     else:
         workers = Workers.objects.all()
         
